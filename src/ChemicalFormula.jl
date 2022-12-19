9d924e69796de68ddd76a4190a9ca316251110ea
tmattick <tmattick@smail.uni-koeln.de>
2022-12-19 22:47:22 +0100

    Expand docstrings for unicode & latex

--- src/ChemicalFormula.jl
+++ src/ChemicalFormula.jl
@@ -94,6 +94,8 @@ const displayforms = Dict(
 )
 
 """
+    unicode(formula, includecharge=true, form="formula")
+
 Give a formula written in unicode characters. `includecharge` determines whether the 
 electronic charge should be given, defaults to `true`. `form` has to be one of `"formula"`
 (the `formula` that was given to construct the `Formula` object), `"hill"` or 
@@ -147,6 +149,8 @@ unicode(formula::Formula, form::AbstractString) = unicode(formula, true, form)
 unicode(formula::Formula) = unicode(formula, true, "formula")
 
 """
+    latex(formula, includecharge=true, form="formula")
+
 Give a formula written in LaTeX, using the mhchem package. `includecharge` determines 
 whether the electronic charge should be given, defaults to `true`. `form` has to be one of 
 `"formula"` (the `formula` that was given to construct the `Formula` object), `"hill"` or 
